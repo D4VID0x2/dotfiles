8d3eb62ce0fc7795adbaa71abf247f36c04e5d27
D4VIDB2 <blaha.david.2018@gmail.com>
2021-04-13 10:51:49 +0200

    sync bashrc

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ cp .zshrc ~/.zshrc
 cp .tmux.conf ~/.tmux.conf
 cp .shell_aliases ~/.config/shell_aliases
 cp .init.nvim ~/.config/nvim/init.vim
+cp .bashrc ~/.bashrc
+
 
 
--- sync.sh
+++ sync.sh
@@ -4,3 +4,4 @@ cp ~/.zshrc ./.zshrc
 cp ~/.tmux.conf ./.tmux.conf
 cp ~/.config/shell_aliases ./.shell_aliases
 cp ~/.config/nvim/init.vim ./.init.nvim
+cp ~/.bashrc ./.bashrc
