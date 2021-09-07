a77e58027502849a57f99ae49658dc27599cd405
D4VIDB2 <blaha.david.2018@gmail.com>
2021-09-07 21:00:17 +0200

    added i3 and polybar

---
+++ setup_zsh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Oh My Zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+
+
+
+
+# Neovim
+
+sudo pacman -S neovim nodejs npm
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
--- sync.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-cp ~/.zshrc ./.zshrc
-cp ~/.tmux.conf ./.tmux.conf
-cp ~/.config/shell_aliases ./.shell_aliases
-cp ~/.config/nvim/init.vim ./.init.nvim
-cp ~/.bashrc ./.bashrc
