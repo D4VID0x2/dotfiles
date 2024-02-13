e20b626bc7cf6c66c85d850d9e989134d06166c3
D4VID <d4vid@d4vid.xyz>
2024-02-13 16:13:26 +0100

    config telescope

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -3,11 +3,11 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
-vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 4 -- insert 4 spaces for a tab
-vim.opt.softtabstop = 4 -- insert 4 spaces for a tab
+vim.opt.shiftwidth = 4        -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4           -- insert 4 spaces for a tab
+vim.opt.softtabstop = 4       -- insert 4 spaces for a tab
 vim.opt.relativenumber = true -- relative line numbers
-vim.opt.wrap = true -- wrap lines
+vim.opt.wrap = true           -- wrap lines
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.mouse = ""
@@ -31,8 +31,8 @@ vim.cmd('source ~/.config/lvim/user.vim')
 
 -- Add extra plugins
 lvim.plugins = {
-  { "octol/vim-cpp-enhanced-highlight" },
-  { "zhimsel/vim-stay" }
+    { "octol/vim-cpp-enhanced-highlight" },
+    { "zhimsel/vim-stay" }
 }
 
 
@@ -40,5 +40,44 @@ lvim.plugins = {
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "svls" })
 -- remove `svlangserver` from `skipped_servers` list
 lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
-  return server ~= "svlangserver"
+    return server ~= "svlangserver"
 end, lvim.lsp.automatic_configuration.skipped_servers)
+
+vim.cmd('au BufRead,BufNewFile,BufEnter *.ksy set syntax=yaml')
+
+
+-- telescope
+for key, _ in pairs(lvim.builtin.telescope.pickers) do
+    if key ~= "planets" then
+        lvim.builtin.telescope.pickers[key].previewer = nil
+        lvim.builtin.telescope.pickers[key].theme = nil
+        lvim.builtin.telescope.pickers[key].layout_strategy = nil
+        lvim.builtin.telescope.pickers[key].layout_config = nil
+    end
+end
+
+lvim.builtin.telescope.defaults.prompt_prefix = "  "
+lvim.builtin.telescope.defaults.selection_caret = "❯ "
+-- lvim.builtin.telescope.defaults.winblend = 10
+lvim.builtin.telescope.defaults.borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+lvim.builtin.telescope.defaults.layout_strategy = "horizontal"
+lvim.builtin.telescope.defaults.layout_config = {
+    horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+        results_width = 0.8,
+    },
+    vertical = {
+        mirror = false,
+    },
+    width = 0.87,
+    height = 0.80,
+    preview_cutoff = 120,
+}
+
+lvim.builtin.which_key.mappings["f"] = {
+    function()
+        require("lvim.core.telescope.custom-finders").find_project_files { previewer = true }
+    end,
+    "Find File",
+}
