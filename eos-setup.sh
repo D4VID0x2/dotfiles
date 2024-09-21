e2fae448f8cc76b9adcabe8632df7789a3d16fbf
D4VID <d4vid@d4vid.xyz>
2024-09-21 10:34:17 +0200

    Update LunarVim

--- eos-setup.sh
+++ eos-setup.sh
@@ -32,7 +32,7 @@ git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-
 
 # LunarVim
 yay -Sy python-pynvim --needed --noconfirm
-LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
 
 
 # dotfiles
